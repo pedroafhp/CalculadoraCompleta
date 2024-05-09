3dfe6df0e420efb69dbb65b242b5519e83c560a1
PEDRO HENRIQUE FELIX DE ALMEIDA <pedro.hfalmeida3@senacsp.edu.br>
2024-05-08 22:00:02 -0300

    Primeiro Commit

---
+++ CalculadoraCompleta/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace CalculadoraCompleta
+{
+    partial class CalculadoraCompleta
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CalculadoraCompleta));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NumeroUm = new System.Windows.Forms.Button();
+            this.NumeroDois = new System.Windows.Forms.Button();
+            this.NumeroTres = new System.Windows.Forms.Button();
+            this.NumeroQuatro = new System.Windows.Forms.Button();
+            this.NumeroCinco = new System.Windows.Forms.Button();
+            this.NumeroSeis = new System.Windows.Forms.Button();
+            this.NumeroSete = new System.Windows.Forms.Button();
+            this.NumeroOito = new System.Windows.Forms.Button();
+            this.NumeroNove = new System.Windows.Forms.Button();
+            this.NumeroZero = new System.Windows.Forms.Button();
+            this.Soma = new System.Windows.Forms.Button();
+            this.Subtrair = new System.Windows.Forms.Button();
+            this.Multiplicar = new System.Windows.Forms.Button();
+            this.Resultado = new System.Windows.Forms.Button();
+            this.Dividir = new System.Windows.Forms.Button();
+            this.Cancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // NumeroUm
+            // 
+            resources.ApplyResources(this.NumeroUm, "NumeroUm");
+            this.NumeroUm.Name = "NumeroUm";
+            this.NumeroUm.UseVisualStyleBackColor = true;
+            this.NumeroUm.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NumeroDois
+            // 
+            resources.ApplyResources(this.NumeroDois, "NumeroDois");
+            this.NumeroDois.Name = "NumeroDois";
+            this.NumeroDois.UseVisualStyleBackColor = true;
+            this.NumeroDois.Click += new System.EventHandler(this.NumeroDois_Click);
+            // 
+            // NumeroTres
+            // 
+            resources.ApplyResources(this.NumeroTres, "NumeroTres");
+            this.NumeroTres.Name = "NumeroTres";
+            this.NumeroTres.UseVisualStyleBackColor = true;
+            this.NumeroTres.Click += new System.EventHandler(this.NumeroTres_Click);
+            // 
+            // NumeroQuatro
+            // 
+            resources.ApplyResources(this.NumeroQuatro, "NumeroQuatro");
+            this.NumeroQuatro.Name = "NumeroQuatro";
+            this.NumeroQuatro.UseVisualStyleBackColor = true;
+            this.NumeroQuatro.Click += new System.EventHandler(this.NumeroQuatro_Click);
+            // 
+            // NumeroCinco
+            // 
+            resources.ApplyResources(this.NumeroCinco, "NumeroCinco");
+            this.NumeroCinco.Name = "NumeroCinco";
+            this.NumeroCinco.UseVisualStyleBackColor = true;
+            this.NumeroCinco.Click += new System.EventHandler(this.NumeroCinco_Click);
+            // 
+            // NumeroSeis
+            // 
+            resources.ApplyResources(this.NumeroSeis, "NumeroSeis");
+            this.NumeroSeis.Name = "NumeroSeis";
+            this.NumeroSeis.UseVisualStyleBackColor = true;
+            this.NumeroSeis.Click += new System.EventHandler(this.NumeroSeis_Click);
+            // 
+            // NumeroSete
+            // 
+            resources.ApplyResources(this.NumeroSete, "NumeroSete");
+            this.NumeroSete.Name = "NumeroSete";
+            this.NumeroSete.UseVisualStyleBackColor = true;
+            this.NumeroSete.Click += new System.EventHandler(this.NumeroSete_Click);
+            // 
+            // NumeroOito
+            // 
+            resources.ApplyResources(this.NumeroOito, "NumeroOito");
+            this.NumeroOito.Name = "NumeroOito";
+            this.NumeroOito.UseVisualStyleBackColor = true;
+            this.NumeroOito.Click += new System.EventHandler(this.NumeroOito_Click);
+            // 
+            // NumeroNove
+            // 
+            resources.ApplyResources(this.NumeroNove, "NumeroNove");
+            this.NumeroNove.Name = "NumeroNove";
+            this.NumeroNove.UseVisualStyleBackColor = true;
+            this.NumeroNove.Click += new System.EventHandler(this.NumeroNove_Click);
+            // 
+            // NumeroZero
+            // 
+            resources.ApplyResources(this.NumeroZero, "NumeroZero");
+            this.NumeroZero.Name = "NumeroZero";
+            this.NumeroZero.UseVisualStyleBackColor = true;
+            this.NumeroZero.Click += new System.EventHandler(this.NumeroZero_Click);
+            // 
+            // Soma
+            // 
+            resources.ApplyResources(this.Soma, "Soma");
+            this.Soma.Name = "Soma";
+            this.Soma.UseVisualStyleBackColor = true;
+            this.Soma.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Subtrair
+            // 
+            resources.ApplyResources(this.Subtrair, "Subtrair");
+            this.Subtrair.Name = "Subtrair";
+            this.Subtrair.UseVisualStyleBackColor = true;
+            this.Subtrair.Click += new System.EventHandler(this.Subtrair_Click);
+            // 
+            // Multiplicar
+            // 
+            resources.ApplyResources(this.Multiplicar, "Multiplicar");
+            this.Multiplicar.Name = "Multiplicar";
+            this.Multiplicar.UseVisualStyleBackColor = true;
+            this.Multiplicar.Click += new System.EventHandler(this.Multiplicar_Click);
+            // 
+            // Resultado
+            // 
+            resources.ApplyResources(this.Resultado, "Resultado");
+            this.Resultado.Name = "Resultado";
+            this.Resultado.UseVisualStyleBackColor = true;
+            this.Resultado.Click += new System.EventHandler(this.Resultado_Click);
+            // 
+            // Dividir
+            // 
+            resources.ApplyResources(this.Dividir, "Dividir");
+            this.Dividir.Name = "Dividir";
+            this.Dividir.UseVisualStyleBackColor = true;
+            this.Dividir.Click += new System.EventHandler(this.Dividir_Click);
+            // 
+            // Cancelar
+            // 
+            resources.ApplyResources(this.Cancelar, "Cancelar");
+            this.Cancelar.Name = "Cancelar";
+            this.Cancelar.UseVisualStyleBackColor = true;
+            this.Cancelar.Click += new System.EventHandler(this.Cancelar_Click);
+            // 
+            // CalculadoraCompleta
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Controls.Add(this.Cancelar);
+            this.Controls.Add(this.Dividir);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.Multiplicar);
+            this.Controls.Add(this.Subtrair);
+            this.Controls.Add(this.Soma);
+            this.Controls.Add(this.NumeroZero);
+            this.Controls.Add(this.NumeroNove);
+            this.Controls.Add(this.NumeroOito);
+            this.Controls.Add(this.NumeroSete);
+            this.Controls.Add(this.NumeroSeis);
+            this.Controls.Add(this.NumeroCinco);
+            this.Controls.Add(this.NumeroQuatro);
+            this.Controls.Add(this.NumeroTres);
+            this.Controls.Add(this.NumeroDois);
+            this.Controls.Add(this.NumeroUm);
+            this.Controls.Add(this.textBox1);
+            this.Name = "CalculadoraCompleta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Button NumeroUm;
+        private System.Windows.Forms.Button NumeroDois;
+        private System.Windows.Forms.Button NumeroTres;
+        private System.Windows.Forms.Button NumeroQuatro;
+        private System.Windows.Forms.Button NumeroCinco;
+        private System.Windows.Forms.Button NumeroSeis;
+        private System.Windows.Forms.Button NumeroSete;
+        private System.Windows.Forms.Button NumeroOito;
+        private System.Windows.Forms.Button NumeroNove;
+        private System.Windows.Forms.Button NumeroZero;
+        private System.Windows.Forms.Button Soma;
+        private System.Windows.Forms.Button Subtrair;
+        private System.Windows.Forms.Button Multiplicar;
+        private System.Windows.Forms.Button Resultado;
+        private System.Windows.Forms.Button Dividir;
+        private System.Windows.Forms.Button Cancelar;
+    }
+}
+
---
+++ CalculadoraCompleta/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraCompleta
+{
+    public partial class CalculadoraCompleta : Form
+    {
+        Model modelo;
+        public CalculadoraCompleta()
+        {
+            modelo = new Model();
+            InitializeComponent();
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            {
+                if (NumeroUm.Text == "" || NumeroDois.Text == "")
+                {
+                    Resultado.Text = "Preencha os Campos Numéricos";
+                }
+                else
+                {
+                    int num1 = Convert.ToInt32(NumeroUm.Text);
+                    int num2 = Convert.ToInt32(NumeroDois.Text);
+                    Resultado.Text = "" + modelo.Somar(num1, num2);
+                }
+            }
+        }//Fim do Somar
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void NumeroDois_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroTres_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroQuatro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroCinco_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroSeis_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroSete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroOito_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroNove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroZero_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Subtrair_Click(object sender, EventArgs e)
+        {
+            if (NumeroUm.Text == "" || NumeroDois.Text == "")
+            {
+                Resultado.Text = "Preencha os Campos Numéricos";
+            }
+            else
+            {
+                int num1 = Convert.ToInt32(NumeroUm.Text);
+                int num2 = Convert.ToInt32(NumeroDois.Text);
+                Resultado.Text = "" + modelo.Subtrair(num1, num2);
+            }
+        }//Fim do Subtrair
+
+        private void Multiplicar_Click(object sender, EventArgs e)
+        {
+            if (NumeroUm.Text == "" || NumeroDois.Text == "")
+            {
+                Resultado.Text = "Preencha os Campos Numéricos";
+            }
+            else
+            {
+                int num1 = Convert.ToInt32(NumeroUm.Text);
+                int num2 = Convert.ToInt32(NumeroDois.Text);
+                Resultado.Text = "" + modelo.Multiplicar(num1, num2);
+            }
+        }//Fim do Multiplicar
+
+        private void Dividir_Click(object sender, EventArgs e)
+        {
+            {
+                if (NumeroUm.Text == "" || NumeroDois.Text == "")
+                {
+                    Resultado.Text = "Preencha os Campos Numéricos";
+                }
+                else
+                {
+                    int num1 = Convert.ToInt32(NumeroUm.Text);
+                    int num2 = Convert.ToInt32(NumeroDois.Text);
+                    if (modelo.Dividir(num1, num2) == -1)
+                    {
+                        Resultado.Text = "Impossível Dividir";
+                    }
+                    else
+                    {
+                        Resultado.Text = "" + modelo.Dividir(num1, num2);
+                    }
+                }
+            }
+        }//Fim do Dividir
+
+        private void Resultado_Click(object sender, EventArgs e)
+        {
+            
+        }//Fim do Resultado
+
+        private void Cancelar_Click(object sender, EventArgs e)
+        {
+            if (NumeroUm.Text == "" || NumeroDois.Text == "")
+            {
+                Resultado.Text = "Preencha os Campos Numéricos";
+            }
+            else
+            {
+                NumeroUm.Text = "";
+                NumeroDois.Text = "";
+                Resultado.Text = "";
+            }
+        }//Fim do Cancelar
+
+    }//Fim da Classe
+}//Fim do Projeto
